6a5a5d207905565ba99c2a21c537173c84a373fa
JordyC++n <jcuan4@gmail.com>
2015-08-20 23:59:30 -0500

    Notificaciones de la UI funcionando. (Hay que arreglar el pequeño icono de la barra de notificaciones)

--- app/src/main/java/com/pid2/tata/gcm/GcmMessageHandler.java
+++ app/src/main/java/com/pid2/tata/gcm/GcmMessageHandler.java
@@ -1,22 +1,26 @@
 package com.pid2.tata.gcm;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
-
-import java.util.Set;
+import com.pid2.tata.MainActivity;
+import com.pid2.tata.R;
 
 /**
  * Created by JordyCuan on 19/08/15.
  */
 public class GcmMessageHandler extends IntentService {
 
-	String mes;
 	private Handler handler;
 
 	public GcmMessageHandler() {
@@ -56,7 +60,6 @@ public class GcmMessageHandler extends IntentService {
 
 
 
-
 		// TODO: De aquí en adelante se procesan los datos enviados. Es recomentable usar una DB
 
 
@@ -69,31 +72,65 @@ public class GcmMessageHandler extends IntentService {
 		String messageType = gcm.getMessageType(intent);
 
 
-		mes = extras.getString("fecha");
+		// TODO: Usadas?
+		String fecha = extras.getString("fecha");
+		String doctor = extras.getString("doctor");
+		String paciente = extras.getString("paciente");
+		String comentarios = extras.getString("comentarios");
 
 
-		Set<String> llaves = extras.keySet();
-		String which = "";
-		for (String llave : llaves) {
-			which += llave + ", ";
-		}
+//		Set<String> llaves = extras.keySet();
+//		String which = "";
+//		for (String llave : llaves) {
+//			which += llave + ", ";
+//		}
+//
+//		Log.i("*** GCM ***", which);
 
-		Log.i("*** GCM ***", which);
-
-		showToast();
-		Log.i("*** GCM ***", "Received : (" + messageType + ")  " + extras.getString("title"));
+		// Como nosotros no lo procesamos aquí, lo mandamos a las notificaciones y a la
+		// actividad principal (aunque lo ideal sería aprovechar este otro hilo para
+		// meterlo dentro de una DB).
+		mostrarNotificacion(extras);
+//		Log.i("*** GCM ***", "Received : (" + messageType + ")  " + extras.getString("title"));
 
 		GcmBroadcastReceiver.completeWakefulIntent(intent);
 	}
 
-	public void showToast() {
+	public void mostrarNotificacion(final Bundle extras) {
 
 		// Intervenimos en el UI
 		handler.post(new Runnable() {
 			public void run() {
-				Toast.makeText(getApplicationContext(), mes, Toast.LENGTH_LONG).show();
+				NotificationCompat.Builder
+						mBuilder = new NotificationCompat.Builder(getApplicationContext())
+								.setSmallIcon(R.drawable.ic_medico)
+								.setLargeIcon((((BitmapDrawable)getResources().getDrawable(R.drawable.ic_medico)).getBitmap()))
+								// TODO: Cambiar estos 2 despues
+								.setContentTitle("Nuevo Reporte Médico.")
+								.setContentText("El paciente " + extras.getString("paciente") +
+										", tiene un nuevo reporte del médico " + extras.getString("doctor"))
+								//.setContentInfo("4")
+								.setAutoCancel(true)
+								.setDefaults(Notification.DEFAULT_ALL)
+								.setPriority(NotificationCompat.PRIORITY_HIGH)
+								.setTicker("Alerta!");
+
+				Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+				// Como no empleamos (por ahora) los extras, los mandamos a la otra actividad
+				// para su manipulación
+				intent.putExtras(extras);
+
+				PendingIntent contIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+
+				mBuilder.setContentIntent(contIntent);
+
+				NotificationManager mNotificationManager =
+						(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+				final int NOTIF_ALERTA_ID = 123;  // Identificador único de nuestra notificación
+				mNotificationManager.notify(NOTIF_ALERTA_ID, mBuilder.build());
 			}
 		});
-
 	}
 }
